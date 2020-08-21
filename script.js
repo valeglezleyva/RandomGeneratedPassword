324524ced4f52db70d10b9d67f9dc4ce5986556a
Valeria Gonzalez Leyva <valeglez.leyva@gmail.com>
2020-08-21 00:33:14 -0500

    Added data storing, validation, for loops.

--- script.js
+++ script.js
@@ -1,12 +1,91 @@
 //Assign array of elements password will include
-var PasswordMax = [1,2,3,4,5,5,6,7,8,9]
 var lowerChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var numberChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; 
 var specialChar = ['!','@','#','$','%','^','&','*','(',')','+','-','=','~','<','>','?'];
 
+function passwordCriteria() {
+
+  var charLength = parseInt(prompt("Please enter a number between 8-128 characters for your new password"));
+  
+    if (charLength < 8 || charLength > 128) {
+      alert("You must choose a number between 8-128. Please try again")
+      return;
+    } 
+  var lowercase = confirm("Do you want lowercase?");
+  var uppercase = confirm("Do you want uppercase?");
+  var numbers = confirm("Do you want numbers?");
+  var specialCharacters = confirm("Do you want special characters?");
+  
+  // Validate data
+    if (charLength === false && lowercase === false && uppercase === false && numbers === false && specialCharacters === false) {
+      alert("You must select at least one of the character types");
+      return;
+    }
+
+  // Store user input
+  var userInput = {
+    charLength: charLength,
+    lowercase: lowercase,
+    uppercase: uppercase,
+    numbers: numbers,
+    specialCharacters: specialCharacters
+  };
+  return userInput;
+}
+
+// Randomize characters
+function getRandomChars(arr) {
+  var randomIndex = Math.floor(Math.random()*arr.length);
+  var randomEl = arr[randomIndex];
+  
+  return randomEl;
+}
+
+//Generate Password
+function generatePassword() {
+  var inputChars = passwordCriteria();
+  var result = [];
+
+  var possibleChars = [];
+  var guaranteedChars = [];
+
+  if (inputChars.lowercase) {
+    possibleChars = possibleChars.concat(lowerChar);
+    guaranteedChars.push(getRandomChars(lowerChar));
+  }
+
+  if (inputChars.uppercase) {
+    possibleChars = possibleChars.concat(upperChar);
+    guaranteedChars.push(getRandomChars(upperChar));
+  }
+
+  if (inputChars.numbers) {
+    possibleChars = possibleChars.concat(numberChar);
+    guaranteedChars.push(getRandomChars(numberChar));
+  }
+
+  if (inputChars.specialCharacters) {
+    possibleChars = possibleChars.concat(specialChar);
+    guaranteedChars.push(getRandomChars(specialChar));
+  }
+
+  for (var i = 0; i < inputChars.charLength; i++) {
+    var possibleChar = getRandomChars(possibleChars);
+    result.push(possibleChar);
+  }
+
+  for (var i = 0; i < guaranteedChars.length; i++) {
+    result[i] = guaranteedChars[i];
+  }
+  
+  return result.join('');
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -14,52 +93,4 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// Functions
-// for(var i = 0; i < PasswordMax.length; i++){}
-
-function generatePassword() {
-
-  var lowerCharsIndex = Math.floor(Math.random()*lowerChar.length);
-  var upperCharsIndex = Math.floor(Math.random()*upperChar.length);
-  var numberCharsIndex = Math.floor(Math.random()*numberChar.length);
-  var specialCharsIndex = Math.floor(Math.random()*specialChar.length);
-
-  var charLength = prompt("Please enter a number between 8-128 characters for your new password");
-    if (charLength < 8 || charLength > 128) {
-      alert("You must choose a number between 8-128. Please try again")
-      return false;
-    }
-
-  var lowercase = confirm("Do you want lowercase?");
-    if (lowercase) {
-      for (var i = 0; i < lowerChar.length; i++) {
-        var lowerChars = lowerChar[lowerCharsIndex];
-      }
-      console.log(lowerChars);
-    }
-
-  var uppercase = confirm("Do you want uppercase?");
-    if (uppercase) {
-      var upperChars = upperChar[upperCharsIndex];
-      console.log (upperChars);
-    }
-
-  var numbers = confirm("Do you want numbers?");
-    if (numbers) {
-      var numberChars = numberChar[numberCharsIndex];
-      console.log (numberChars);
-    }
-
-  var specialCharacters = confirm("Do you want special characters?");
-  if (specialCharacters) {
-    var specialChars = specialChar[specialCharsIndex];
-    console.log (specialChars);
-  }
-}
-
